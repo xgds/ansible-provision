38582bc9b5e15705d1ecba0869dc7c285284f5a4
David Lees <david.lees@nasa.gov>
2017-03-14 21:31:27 +0000

    add dev settings for dev instances

---
+++ code-prep/devDbSettings.sql
@@ -0,0 +1,4 @@
+update xgds_core_constant set value="127.0.0.1" where name="EV1_TRACKING_IP";
+update xgds_core_constant set value="127.0.0.1" where name="EV2_TRACKING_IP";
+update xgds_core_constant set value="TCP" where name="EV1_TRACKING_PROTO";
+update xgds_core_constant set value="TCP" where name="EV2_TRACKING_PROTO";
