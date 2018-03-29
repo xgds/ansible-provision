741ff3626cea110fa882c99f5353cf80e43b2889
Tamar Cohen <tamar.cohen@nasa.gov>
2018-03-29 16:58:43 -0700

    added nodejs 8

--- prep-provisioning.sh
+++ prep-provisioning.sh
@@ -22,6 +22,7 @@
 
 sudo usermod -a -G staff $USER
 sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-add-repository https://deb.nodesource.com/setup_8.x
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install ansible
